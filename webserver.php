269e298f787582d7705edba39645b46961e86e65
MadBad <madbad82@gmail.com>
2012-12-06 22:48:51 +0100

    initial commit

---
+++ webserver.php
@@ -0,0 +1,40 @@
+<?php
+//error_reporting( E_ALL | E_STRICT );
+//error_reporting(E_ERROR | E_WARNING | E_PARSE | E_COMPILE_ERROR);
+//ini_set('display_errors','On');
+
+//require_once './config.inc.php';
+//require_once './database.php';
+
+/*
+$myDb=new DataBase($config->database);
+$myDb->query('
+	from: tablename
+	select: id,name,data
+	conditions:[
+		operator = and,
+		id = test,
+		data = bho,
+		ciro > me
+	]
+');
+*/
+
+//$myXml=simplexml_load_file ('./xml/hostlist.xml');
+
+
+
+//var_dump($myXml);
+
+//echo $myXml->date["validateAs"];
+echo $myXml->asXML();
+
+
+function traverseXmlObj($xml){
+	foreach($xml->children() as $elementName => $child){
+		echo $elementName;
+	}
+}
+echo 'test';
+traverseXmlObj($myXml);
+?>
